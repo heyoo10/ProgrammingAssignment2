f1c756b616c05df4163a6a2c197d6d534521b11c
Blaine <blainecloverr@gmail.com>
2021-07-05 01:06:45 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,14 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+hello
+this is my lexical scoping assignment
+I am learning about R Programming
+#this is a hashtag
+#hi
+#hey
+#hello
+#practice only
 makeCacheMatrix <- function(x = matrix()) {
 
 }
